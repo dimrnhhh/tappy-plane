04efad46b152248fa330d0581285e664f9643324
Dimas Rian Hastedi <83242712+dimrnhhh@users.noreply.github.com>
2024-07-09 07:56:04 +0700

    Add ground and rock

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var rock_scene: PackedScene
+
 var game_running: bool
 var game_over: bool
 var scroll
@@ -7,11 +9,13 @@ var score
 const SCROLL_SPEED: int = 4
 var screen_size: Vector2i
 var ground_height: int
-var pipes: Array
-const PIPE_DELAY: int = 100
-const PIPE_RANGE: int = 200
+var rocks: Array
+const ROCK_DELAY: int = 200
+const ROCK_RANGE: int = 100
 
 func _ready():
+	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 
 func new_game():
@@ -19,6 +23,8 @@ func new_game():
 	game_over = false
 	score = 0
 	scroll = 0
+	rocks.clear()
+	generate_rocks()
 	$Plane.reset();
 	
 func _input(event):
@@ -35,3 +41,28 @@ func start_game():
 	game_running = true
 	$Plane.flying = true
 	$Plane.flap()
+	$RockTimer.start()
+
+func _process(delta):
+	if game_running:
+		scroll += SCROLL_SPEED
+		if scroll >= screen_size.x:
+			scroll = 0
+		$Ground.position.x = -scroll
+		for rock in rocks:
+			rock.position.x -= SCROLL_SPEED
+
+
+func _on_rock_timer_timeout():
+	generate_rocks()
+
+func generate_rocks():
+	var rock = rock_scene.instantiate()
+	rock.position.x = screen_size.x + ROCK_DELAY
+	rock.position.y = (screen_size.y - ground_height) / 2  + randi_range(-ROCK_RANGE, ROCK_RANGE)
+	rock.hit.connect(rock_hit)
+	add_child(rock)
+	rocks.append(rock)
+
+func rock_hit():
+	pass
